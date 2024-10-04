6c94401c2e59a6240603b8ab9bdbd2091acbae89
ashhad <syedashhad17@gmail.com.com>
2024-10-04 20:33:20 +0500

    login added

--- MERN Project/Backend/Controller/authController.js
+++ MERN Project/Backend/Controller/authController.js
@@ -1,26 +1,43 @@
-const User  = require("../Model/userSchema");
+const User = require("../Model/userSchema");
+const bcrypt = require('bcrypt')
 
-const Login = async(req , res) => {
-    try {
-        res.status(200).send("Welcome to the login page")
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-const Registration = async(req , res) => {
+const Login = async (req, res) => {
     try {
-        const {username, email, password, phone} = req.body;
-        const userExist = await User.findOne({email});
-        if(userExist){
-            return res.status(400).send("User Already Exist");
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ error: "User not found" });
         }
-        const userCreated = User.create({username, email, password, phone});
-        res.status(201).json({msg:userCreated});
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            res.status(200).json({ success: "Logged in" });
+        } else {
+            res.status(400).json({ error: "Invalid credentials" });
+        }
     } catch (error) {
-        console.log(error)
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+const Registration = async (req, res) => {
+    try {
+        const { username, email, password, phone } = req.body;
+        const userEmailExist = await User.findOne({ email });
+        const userUsernameExist = await User.findOne({ username });
+        if (userEmailExist && userUsernameExist) {
+            return res.status(400).json({ error: "User Already Exist" });
+        }
+        const encodedPassword = await bcrypt.hash(password, 6);
+        const userCreated = await User.create({ username, email, password: encodedPassword, phone });
+        return res.status(201).json({ userDetails: userCreated });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
     }
 }
 
 
-module.exports = {Login, Registration};
+module.exports = { Login, Registration };
--- MERN Project/Backend/Routes/authRoutes.js
+++ MERN Project/Backend/Routes/authRoutes.js
@@ -3,7 +3,7 @@ const { Login, Registration } = require('../Controller/authController');
 
 const authRoutes = express.Router();
 
-authRoutes.get('/login',Login)
+authRoutes.post('/login',Login)
 
 authRoutes.post('/registration',Registration)
 
