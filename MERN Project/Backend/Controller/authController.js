47534968a91c0452b9850555ff8a2a566de74465
Syed Ashhad <syedashhad17@gmail.com>
2024-10-05 10:09:17 +0500

    mongo db atas connection added

--- MERN Project/Backend/Controller/authController.js
+++ MERN Project/Backend/Controller/authController.js
@@ -1,42 +1,45 @@
 const User = require("../Model/userSchema");
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
 
 const Login = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(400).json({ error: "User not found" });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (isPasswordValid) {
-            res.status(200).json({ success: "Logged in" });
-        } else {
-            res.status(400).json({ error: "Invalid credentials" });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
     }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.status(200).json({ success: "Logged in" });
+    } else {
+      res.status(400).json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const Registration = async (req, res) => {
-    try {
-        const { username, email, password, phone } = req.body;
-        const userEmailExist = await User.findOne({ email });
-        const userUsernameExist = await User.findOne({ username });
-        if (userEmailExist && userUsernameExist) {
-            return res.status(400).json({ error: "User Already Exist" });
-        }
-        const encodedPassword = await bcrypt.hash(password, 6);
-        const userCreated = await User.create({ username, email, password: encodedPassword, phone });
-        return res.status(201).json({ userDetails: userCreated });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal server error" });
+  try {
+    const { username, email, password, phone } = req.body;
+    const userEmailExist = await User.findOne({ email });
+    const userUsernameExist = await User.findOne({ username });
+    if (userEmailExist && userUsernameExist) {
+      return res.status(400).json({ error: "User Already Exist" });
     }
-}
-
+    const encodedPassword = await bcrypt.hash(password, 6);
+    const userCreated = await User.create({
+      username,
+      email,
+      password: encodedPassword,
+      phone,
+    });
+    return res.status(201).json({ userDetails: userCreated });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = { Login, Registration };
--- MERN Project/Backend/DB/db.js
+++ MERN Project/Backend/DB/db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDb = async () =>{
+const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/mern");
+        // await mongoose.connect("mongodb://localhost:27017/mern");
+        await mongoose.connect("mongodb+srv://ashhad12009:NvYnk6I7qAAWGVjx@userlist.ubuai.mongodb.net/userList?retryWrites=true&w=majority");
+        console.log("Database connected successfully");
     } catch (error) {
-        console.error("connection Falied");
-        process.exit();
+        console.error("Connection Failed", error);
+        process.exit(1);
     }
-}
+};
+
 module.exports = connectDb;
